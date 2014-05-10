952da5a38b1cca49135bc903ddd50a19abe5e01c
buena <buena@scala-98Labs.(none)>
2014-05-10 22:15:58 +0800

    partial commit add report1 for marker and infowindow sample

--- routes/maps/index.js
+++ routes/maps/index.js
@@ -1,3 +1,4 @@
 module.exports = function(router) {
   router.get('/reports', require('./reports'));
+  router.get('/report1', require('./report1'));
 };
---
+++ routes/maps/report1.js
@@ -0,0 +1,3 @@
+module.exports = function(req, res) {
+  	res.render('public/maps/report1', { title: 'User reports' });
+};
