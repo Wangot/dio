a3e659df2747d837a0fef9559719cd0566e2469e
erick <erickson.leynes@98labs.com>
2014-05-10 16:12:38 +0800

    removed unessary code

--- library/dio/utilities/index.js
+++ library/dio/utilities/index.js
@@ -1,7 +1,3 @@
 exports.config = require('./config');
 exports.db = require('./db');
-<<<<<<< HEAD
-//exports.authentication = require('./authentication');
-=======
 exports.message = require('./message');
->>>>>>> partial commit for api structure
